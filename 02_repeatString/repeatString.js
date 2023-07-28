b65313cf6e142cb42fe816eddc98b0762f207dc8
Gawdlie <maverickgm9@gmail.com>
2023-07-28 14:38:56 -0700

    Fix javascript exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeatedString = "";
+
+    while(num > 0) {
+        repeatedString  += string;
+        num--;
+
+    }
+    return repeatedString;
 
 };
 
